df0aec2743b6641f968d44842f09e8b850589648
k-songs <ad120307@gmail.com>
2025-09-08 15:59:19 +0900

    Add and modify files

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-
-import { StyleSheet} from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
-
-
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <CategoriesScreen />
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="m_categories" component={CategoriesScreen}/>
+          <Stack.Screen name="Overview" component={MealsOverviewScreen}/>
+        </Stack.Navigator>
+   
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,29 +1,37 @@
-import { FlatList, View, StyleSheet } from "react-native";
+import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
 
-function CategoriesScreen() {
+function CategoriesScreen({ navigation })     {
+  function renderCategoryItem(itemData) {
+    function pressHandler(){
+      navigation.navigate('Overview',{
+      categoryId: itemData.item.id
+      });
+    }
+
   return (
-    <View style={styles.container}>   <FlatList
-    data={CATEGORIES}
-    keyExtractor={(item) => item.id}
-    renderItem={renderCategoryItem}
-  /></View>
- 
+    <CategoryGridTile
+      title={itemData.item.title}
+      color={itemData.item.color}
+      onPress={pressHandler}
+    />
+  );
+  }
+  return (
+
+    
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+
   );
 }
 
 export default CategoriesScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
+
